e01816a1acc2554e2705c43aa30bf0d161e82861
hojay <jeahonglee@gmail.com>
2024-03-30 18:49:34 +0900

    WalletConnect and Wagmi setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,65 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import { createWeb3Modal } from "@web3modal/wagmi/react";
+import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
+import { WagmiProvider } from "wagmi";
+import {
+  astarZkatana,
+  mainnet,
+  astar,
+  neonDevnet,
+  neonMainnet,
+  klaytn,
+  sepolia,
+} from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// 0. Setup queryClient
+const queryClient = new QueryClient();
+
+// 1. Get projectId at https://cloud.walletconnect.com
+const projectId = import.meta.env.VITE_WC_PROJECT_ID;
+
+// 2. Create wagmiConfig
+const metadata = {
+  name: "Ailice",
+  description: "Ailice in Cryptoland",
+  url: "https://localhost", // origin must match your domain & subdomain
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+const chains = [
+  mainnet,
+  astarZkatana,
+  astar,
+  neonDevnet,
+  neonMainnet,
+  klaytn,
+  sepolia,
+];
+const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+});
+
+// 3. Create modal
+createWeb3Modal({
+  wagmiConfig: config,
+  projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  enableOnramp: true, // Optional - false as default
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </WagmiProvider>
+  </React.StrictMode>
+);
